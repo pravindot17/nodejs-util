4278528a638f86f1bda8bf2fb2590ab4691a99c1
Pravin <pravin.lolage@b2x.com>
2018-12-11 17:26:08 +0530

    added string functions

--- example/test/index.js
+++ example/test/index.js
@@ -1,83 +1,109 @@
-let chai = require('chai'),
-    expect = chai.expect,
-    jsUtils = require('nodejs-util');
+let chai = require('chai')
+let expect = chai.expect
+let jsUtils = require('nodejs-util')
 
-chai.use(require('chai-like'));
-chai.use(require('chai-things'));
+chai.use(require('chai-like'))
+chai.use(require('chai-things'))
 
-describe(('Javascript Utils'), () => {
-    let input =  null;
-    describe('*** POSITIVE CASES ***', () => {
-        it('isNull function', () => {
-            input = null;
-            expect(jsUtils.isNull(input)).equals(true);
-        });
-
-        it('isNotNull function', () => {
-            input = 'I am a bad guy';
-            expect(jsUtils.isNotNull(input)).equals(true);
-        });
-
-        it('isArray function', () => {
-            input = [];
-            expect(jsUtils.isArray(input)).equals(true);
-        });
-
-        it('isFilledArray function', () => {
-            input = [1, 2, 3];
-            expect(jsUtils.isFilledArray(input)).equals(true);
-        });
-
-        it('isEmpty function', () => {
-            input = '';
-            expect(jsUtils.isEmpty(input)).equals(true);
-        });
-
-        it('isNotEmpty function', () => {
-            input = 'Who said I am empty';
-            expect(jsUtils.isNotEmpty(input)).equals(true);
-        });
-
-        it('formatNumber function', () => {
-            input = 1000.1256;
-            expect(jsUtils.formatNumber(input)).equals(1000.13);
-        });
-
-        it('sleep function', async () => {
-            await jsUtils.sleep(1000)
-            expect('It worked').equals('It worked');
-        });
+describe('Javascript Utils', () => {
+  let input = null
+  describe('*** NULL CASES ***', () => {
+    it('positive isNull function', () => {
+      input = null
+      expect(jsUtils.isNull(input)).equals(true)
     })
 
-    describe('*** BAD CASES ***', () => {
-        it('isNull function', () => {
-            input = 'Hi, this is not null';
-            expect(jsUtils.isNull(input)).equals(false);
-        });
-
-        it('isNotNull function', () => {
-            input = null;
-            expect(jsUtils.isNotNull(input)).equals(false);
-        });
-
-        it('isArray function', () => {
-            input = 'Tell me, is this array';
-            expect(jsUtils.isArray(input)).equals(false);
-        });
-
-        it('isFilledArray function', () => {
-            input = [];
-            expect(jsUtils.isFilledArray(input)).equals(false);
-        });
-
-        it('isEmpty function', () => {
-            input = 'Is is empty?';
-            expect(jsUtils.isEmpty(input)).equals(false);
-        });
-
-        it('isNotEmpty function', () => {
-            input = '';
-            expect(jsUtils.isNotEmpty(input)).equals(false);
-        });
+    it('negative isNull function', () => {
+      input = 'Hi, this is not null'
+      expect(jsUtils.isNull(input)).equals(false)
     })
+
+    it('positive isNotNull function', () => {
+      input = 'I am a bad guy'
+      expect(jsUtils.isNotNull(input)).equals(true)
+    })
+
+    it('negative isNotNull function', () => {
+      input = null
+      expect(jsUtils.isNotNull(input)).equals(false)
+    })
+  })
+
+  describe('*** ARRAY CASES ***', () => {
+    it('positive isArray function', () => {
+      input = []
+      expect(jsUtils.isArray(input)).equals(true)
+    })
+
+    it('negative isArray function', () => {
+      input = 'Tell me, is this array'
+      expect(jsUtils.isArray(input)).equals(false)
+    })
+
+    it('positive isFilledArray function', () => {
+      input = [1, 2, 3]
+      expect(jsUtils.isFilledArray(input)).equals(true)
+    })
+
+    it('negative isFilledArray function', () => {
+      input = []
+      expect(jsUtils.isFilledArray(input)).equals(false)
+    })
+  })
+
+  describe('*** empty CASES ***', () => {
+    it('positive isEmpty function', () => {
+      input = ''
+      expect(jsUtils.isEmpty(input)).equals(true)
+    })
+
+    it('negative isEmpty function', () => {
+      input = 'Is is empty?'
+      expect(jsUtils.isEmpty(input)).equals(false)
+    })
+
+    it('positive isNotEmpty function', () => {
+      input = 'Who said I am empty'
+      expect(jsUtils.isNotEmpty(input)).equals(true)
+    })
+
+    it('negative isNotEmpty function', () => {
+      input = ''
+      expect(jsUtils.isNotEmpty(input)).equals(false)
+    })
+  })
+
+  describe('*** STRING CASES ***', () => {
+    it('sanitize function', () => {
+      input = ' hello World '
+      expect(jsUtils.sanitize(input, 'toLowerCase')).equals('hello world')
+    })
+
+    it('toString function', () => {
+      input = 1234
+      expect(jsUtils.toString(input)).equals('1234')
+    })
+
+    it('isEmptyString function', () => {
+      input = ''
+      expect(jsUtils.isEmptyString(input)).equals(true)
+    })
+
+    it('isValidEmail function', () => {
+      input = 'pravind.@123.45'
+      expect(jsUtils.isValidEmail(input)).equals(false)
+    })
+  })
+
+  describe('*** OTHER CASES ***', () => {
+    it('formatNumber function', () => {
+      input = 1000.1256
+      expect(jsUtils.formatNumber(input)).equals(1000.13)
+    })
+
+    it('sleep function', async () => {
+      await jsUtils.sleep(1000)
+      expect('It worked').equals('It worked')
+    })
+  })
 })
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const _ = require('lodash')
+const TO_LOWER_CASE = 'toLowerCase'
+const TO_UPPER_CASE = 'toUpperCase'
 
 module.exports = {
     isNull,
@@ -8,12 +10,15 @@ module.exports = {
     isEmpty,
     isNotEmpty,
     formatNumber,
-    sleep
+    sleep,
+    sanitize,
+    toString,
+    isEmptyString,
+    isValidEmail
 }
 
 function isNull(obj) {
-    if (undefined === obj || null === obj) return true
-    return false
+    return (undefined === obj || null === obj)
 }
 
 function isNotNull(obj) {
@@ -25,8 +30,7 @@ function isArray(array) {
 }
 
 function isFilledArray(arr) {
-    if (isNotNull(arr) && isArray(arr) && arr.length > 0) return true
-    return false
+    return (isNotNull(arr) && isArray(arr) && arr.length > 0)
 }
 
 function isEmpty(obj) {
@@ -39,10 +43,52 @@ function isNotEmpty(obj) {
 
 function formatNumber(num, decimals = 2) {
     let strNum = num.toString()
-    if (strNum.includes('.'))  return Number(num.toFixed(decimals))
-    else return num
+    if (strNum.includes('.')) {
+        return Number(num.toFixed(decimals))
+    } else {
+        return num
+    }
 }
 
 function sleep(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
+    return new Promise((resolve) => setTimeout(resolve, time))
+}
+
+function sanitize(str, strCase) {
+    let sanitizedStr
+    if (!isNull(str)) {
+        sanitizedStr = str.toString().trim()
+        if (!isNull(strCase)) {
+            switch (strCase) {
+                case TO_LOWER_CASE:
+                    sanitizedStr = sanitizedStr.toLowerCase()
+                    break
+
+                case TO_UPPER_CASE:
+                    sanitizedStr = sanitizedStr.toUpperCase()
+                    break
+
+                default:
+                    break
+            }
+        }
+    }
+    return sanitizedStr
+}
+
+function toString(param) {
+    return sanitize(param.toString())
+}
+
+function isEmptyString(str) {
+    return (isNull(str) || '' === str.trim())
+}
+
+function isValidEmail(email) {
+    if (isEmptyString(email)) {
+        return false
+    }
+
+    const sanitizedEmail = sanitize(email, TO_LOWER_CASE)
+    return (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(sanitizedEmail))
 }
